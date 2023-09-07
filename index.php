33c66f6cd30303b0c4a4ff8109bcd47433857794
Ajaiyeoba Ajibola <114266797+Ajaiyeoba@users.noreply.github.com>
2023-09-07 06:24:43 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php   
+//    $dbserver:  "root";
+//    $dbusername = "localhost";
+//    $password = "";
+//     $dbname = "active"
+
+
+    // $conn = new mysqli($dbserver, $dbusername, $password, $dbname);
+    // if ( $conn -> connect_error) {
+    //     die ("connection failed" .$conn -> connect_error);
+    // }
+
+    // if ($conn -> query ($sql) === True)  {
+    //     echo "Table Created "
+    // }
+    // else {
+    //     echo " error creating table" .$conn->error;
+    // }
+
+    // $conn -> close();
+
+    include("");
+    {
+        $fullname = $_GET[''];
+        $dob = $_GET[''];
+        $gender = $_GET[''];
+        $email = $_GET[''];
+        $mobile = $_GET[''];
+        $password = $_GET[''];
+        $results = 
+    }
+
+  ?>
